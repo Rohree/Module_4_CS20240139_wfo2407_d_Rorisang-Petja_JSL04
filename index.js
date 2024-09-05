4e39c970d48180006ba1883a6eb2113c960927bd
rohree <hellororyy880@gmail.com>
2024-09-05 14:10:01 +0200

    Tanery Operators

--- index.js
+++ index.js
@@ -5,14 +5,18 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+(userPreference === "Sugar-free") 
+? console.log(`Sugar-free`)
+: console.log("Regular");
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
-console.log(``);
+(cansLeft >= 5)
+? console.log("We're stocked!")
+: console.log("Time to restock!")
 
 
 // #### Challenge 3: Workout Intensity
@@ -20,7 +24,9 @@ console.log(``);
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
-console.log(`'}`);
+(heartRate >= 100)
+? console.log("Energy levels are high!")
+: console.log("Boost needed!")
 
 
 // #### Challenge 4: Temperature Suitability
@@ -28,7 +34,9 @@ console.log(`'}`);
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
 let currentTemp = 4; // Current temperature in °C
-console.log(``);
+(currentTemp > 5)
+? console.log("Needs a cooler!")
+: console.log("Chilled to perfection!")
 
 
 // #### Challenge 5: Late Night Coding Session
@@ -37,14 +45,16 @@ console.log(``);
 // otherwise, print "Better stick to water."
 
 // Define the current hour in 24-hour format
-let currentHour = 22; 
+let currentHour = 7; 
 
 // Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
 // Otherwise, print "Better stick to water."
 
 
-
+(currentHour >= 7 && currentHour < 24 )
+? console.log("Unleash the beast!")
+: console.log("Better stick to water.")
 
 
 
